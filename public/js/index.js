04c822186dbcbe57c36c9c0ba5c1d66ce571de2e
Thomas Delalonde <tdelalonde@vente-privee.com>
2019-07-02 17:03:48 +0200

    fix bugs

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,13 @@ const scene = document.querySelector('a-scene')
 ;(async function () {
   const origin = document.location.origin
 
-  window.axios.post(`http://${origin}/api/google`, { query: 'mock' }).then(({ data: images }) => {
+  window.axios.post(`${origin}/api/google`, { query: 'mock' }).then(({ data: images }) => {
     for (let i = 0; i < images.length; i++) {
       let customImage = document.createElement('a-customimage')
 
       customImage.setAttribute('custom-image', { width: images[i].width, height: images[i].height, num: i })
       customImage.setAttribute('radius', 10)
-      customImage.setAttribute('src', `http://${origin}:3000/api/image?url=${images[i].src}`)
+      customImage.setAttribute('src', `${origin}:3000/api/image?url=${images[i].src}`)
 
       scene.appendChild(customImage)
     }
